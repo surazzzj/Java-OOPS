34705e308a0e78b7d1d69b1229c098c47b151577
suraj rawat <surajrwat001@gmail.com>
2026-06-04 10:18:23 +0530

    initial commit for files

---
+++ InheritanceWiithOverriding.java
@@ -0,0 +1,43 @@
+class Employee {
+    public String name;
+    public double salary;
+
+    Employee(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    void work() {
+        System.out.println("Employee is working! Employee Details: Name- "
+                + name + ", Salary- " + salary);
+    }
+}
+
+class Manager extends Employee {
+
+    Manager(String name, double salary) {
+        super(name, salary);
+    }
+
+    @Override
+    void work() {
+        System.out.println("Manager is managing the team! Manager Details: Name- "
+                + name + ", Salary- " + salary);
+    }
+
+    void managerWork() {
+        System.out.println("Manager is taking reviews of reports");
+    }
+}
+
+class InheritanceWiithOverriding {
+    public static void main(String[] args) {
+
+        Employee e1 = new Employee("Sam", 40000);
+        e1.work();
+
+        Manager m1 = new Manager("Raj", 80000);
+        m1.work();
+        m1.managerWork();
+    }
+}
